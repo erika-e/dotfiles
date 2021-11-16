3c4a4e21553b1b63add2b95c27829a2d284f0a20
erika-e <erika.swartz@gmail.com>
2021-11-15 22:17:56 -0500

    Make it Possible to Run Setup More Than Once
    Make the directory only if it doesn't exist, remove the repo and recreate

--- setup_mac.sh
+++ setup_mac.sh
@@ -7,12 +7,20 @@
 echo "Installing Homebrew..."
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+# Install git to get the brewfile
+brew install git
+
+# Install git credential manager
+brew tap microsoft/git
+brew install --cask git-credential-manager-core
+
 # Create a directory for repos
-mkdir code
+echo "Creating code directory"
+mkdir -p code
 cd code
 
-#install git to get the brewfile
-brew install git
+# Remove the dotfiles repo if it already exists and restore from the source
+if [ -d dotfiles ]; then rm -rf dotfiles ; fi
 git clone https://github.com/erika-e/dotfiles.git
 cd dotfiles
 
@@ -30,3 +38,7 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 echo "Get .zshrc config, saved at location: $HOME/.zshrc..."
 # assumes this is running in the repo directory, test this first
 #cp .zshrc ~/.zshrc
+
+# Install Extensions for VS Code
+# https://unix.stackexchange.com/questions/149726/how-to-parse-each-line-of-a-text-file-as-an-argument-to-a-command
+# < file tr '\n' '\0' | xargs -0 -I{} command --option {} this shell script can do it
