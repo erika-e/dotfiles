894920374db0209885bacf6b4f696f69eaba4494
erika-e <erika.e.pullum@gmail.com>
2021-11-27 11:51:27 -0500

    Update Extension Installation Order

--- update_dotfiles.sh
+++ update_dotfiles.sh
@@ -22,5 +22,15 @@ cp ~/.zshrc .zshrc
 # Overwrite the extensions list with the currently installed extensions
 code --list-extensions > vscode-extensions.txt
 
+#The lines below to uninstall and all extensions
+# rm -rf ~/.vscode/extensions
+# mkdir ~/.vscode/extensions
+
+# Change the order for installation 
+# Theses two extensions install their own dependencies and need to go first in order
+# https://www.unix.com/shell-programming-and-scripting/238349-move-line-top-file.html
+gsed -i '/innoverio.vscode-dbt-power-user/d;1i\innoverio.vscode-dbt-power-user' vscode-extensions.txt 
+gsed -i '/ms-python.python/d;1i\ms-python.python' vscode-extensions.txt 
+
 # Overwrite settings.json with the local VSCode settings file
 cp "$HOME/Library/Application Support/Code/User/settings.json" settings.json
